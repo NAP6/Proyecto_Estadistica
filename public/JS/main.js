6f86453cbf450c43ae07b4ead68b84d8e75ae769
Nicolas <nicolas.doo@hotmail.com>
2019-11-13 16:39:43 -0500

    Integracion del codigo a un archvo JS

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,11 +1,33 @@
-alert("Cosa");
+var firebaseConfig = {
+    apiKey: "AIzaSyCgaY_dxaX2RfqMPXcgzdsg0XthF94aYX4",
+    authDomain: "datos-climaticos-f76b2.firebaseapp.com",
+    databaseURL: "https://datos-climaticos-f76b2.firebaseio.com",
+    projectId: "datos-climaticos-f76b2",
+    storageBucket: "datos-climaticos-f76b2.appspot.com",
+    messagingSenderId: "328572715982",
+    appId: "1:328572715982:web:22c4d50b22004363d2619f",
+    measurementId: "G-T9FXW8V8ES"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
+function writeUserData() {
+    const indi = firebase.database().ref('Emilio/IndiceUlt');
 
-var Nicolas_bd = firebase.database();
-Nicolas_bd.on('value', function (snapshot) {
-    snapshot.forEach(function(e){
-        var obj=e.val();
-        document.write(obj.IndiceUlt);
-    });
-    //updateStarCount(postElement, snapshot.val());
-});
+    for (i = 1; i < 10; i++) {
+        const ref = firebase.database().ref('Emilio/' + i);
+        ref.on('value', (snapshot) => {
+            const value = {
+                Aire: snapshot.val().Aire_PPM_,
+                Temperatura: snapshot.val().Tempe_Celsius_,
+                Humedad: snapshot.val().Hum_Porcentaje_,
+                Indice_de_calor: snapshot.val().indice_de_calor_Celsius_
+            };
+            alert(i+"\n" + value.Aire + "\n" + value.Humedad + "\n" + value.Temperatura + "\n" + value
+                .Indice_de_calor);
+
+        });
+    }
+}
+writeUserData();
